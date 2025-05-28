5fbce38a49330c4857744c0805e262bb61f6f633
Nikhil-9981 <rathaurnikihl14@gmail.com>
2025-05-28 21:44:06 +0530

    sh file created to run command all at once

---
+++ run_all_agents.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+uvicorn agents.api_agent.main:app --port 8001 & 
+uvicorn agents.scraper_agent.main:app --port 8002 &
+uvicorn agents.retriever_agent.main:app --port 8003 &
+uvicorn agents.language_agent.main:app --port 8004 &
+uvicorn agents.voice_agent.main:app --port 8005 &
+uvicorn agents.orchestrator_agent.main:app --port 8006 &
+wait
